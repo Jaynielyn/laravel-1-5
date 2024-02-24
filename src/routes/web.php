090516fcfa52fcd6cdd3d4635c940c7ff3b46d83
jay <myacc.j2@gmail.com>
2024-02-24 16:06:58 +0900

    first-laravel

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\MiddlewareController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/', [AuthorController::class, 'index']);
 Route::get('/find', [AuthorController::class, 'find']);
@@ -20,3 +22,7 @@ Route::prefix('book')->group(function () {
     Route::post('/add', [BookController::class, 'create']);
 });
 Route::get('/relation', [AuthorController::class, 'relate']);
+Route::get('/middleware', [MiddlewareController::class, 'index']);
+Route::post('/middleware', [MiddlewareController::class, 'post']);
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
